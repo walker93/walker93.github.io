6bda90899bf6fed88a4124f297afead25841fb45
walker93 <walker1993@live.it>
2026-01-06 17:57:35 +0100

    feat: :monocle_face: Aggiunto Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import '../styles/globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import N8nChatComponent from '../components/n8n-chat';
-
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
+      <GoogleAnalytics gaId="G-1299F55DSC" />
       <N8nChatComponent />
       <Component {...pageProps} />
       <Footer />
